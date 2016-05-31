4b676e5ea724d06cc7d3062f5ba4f647ee69fb88
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-05-31 09:43:44 -0600

    add jquery for button

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+$(document).ready(function() {
 
+  var fortuneCookies = [
+    "Conquer your fears or they will conquer you.",
+    "Zach's project had nothing at all to do with this..",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+  ];
 
-var fortuneCookies = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
+  function cookieDeploy() {
+    var idx = Math.floor(Math.random() * fortuneCookies.length);
+    return fortuneCookies[idx];
+  }
 
-var idx = Math.floor(Math.random() * fortuneCookies.length);
-  return fortuneCookies[idx];
+  $('button').click(function() {
+    $('section').html('<h1>' + cookieDeploy() + '</h1>');
+  });
+
+});
